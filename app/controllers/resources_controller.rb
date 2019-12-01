27e68adbe42502ed38b5af72a37a8a55a0b3ad12
Cataline Baert <snuf_kin@yahoo.com>
2019-12-01 12:56:43 +0100

    define method index

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,4 +1,10 @@
+require 'json'
+require 'open-uri'
+
 class ResourcesController < ApplicationController
   def index
+    url = 'https://swapi.co/api/'
+    serialized_root = URI.open(url).read
+    @root_resource = JSON.parse(serialized_root)
   end
 end
