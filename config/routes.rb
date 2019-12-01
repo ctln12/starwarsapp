b7f5a5e1114248a470eab384e82420c36554dd65
Cataline Baert <snuf_kin@yahoo.com>
2019-12-01 13:35:55 +0100

    add new route and refactor using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'resources/index'
   devise_for :users
   root to: 'resources#index'
+
+  resources :resources, only: [ :index, :show ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
