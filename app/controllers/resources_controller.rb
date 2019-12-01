a050234f777d5cdc7c5f788d634adb64954e2b7d
Cataline Baert <snuf_kin@yahoo.com>
2019-12-01 13:36:18 +0100

    define method show

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -7,4 +7,11 @@ class ResourcesController < ApplicationController
     serialized_root = URI.open(url).read
     @root_resource = JSON.parse(serialized_root)
   end
+
+  def show
+    url = "https://swapi.co/api/#{params[:id]}"
+    serialized_resource = URI.open(url).read
+    resource = JSON.parse(serialized_resource)
+    @resource = resource['results']
+  end
 end
