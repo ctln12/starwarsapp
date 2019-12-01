ce30ec95a5071356e3e8f2bcf513df386fa6256a
Cataline Baert <snuf_kin@yahoo.com>
2019-12-01 18:21:07 +0100

    add event listener on resources buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,21 @@
 //= require rails-ujs
 //= require_tree .
 
-fetchResources();
+// fetchResources();
+
+const results = document.getElementById('results');
+
+const buttons = document.querySelectorAll('.btn-dark');
+
+buttons.forEach((button) =>
+  button.addEventListener('click', (event) => {
+    if (event.target.classList.contains('active')) {
+      event.target.classList.remove('active');
+    } else {
+      document.querySelector('.active').classList.remove('active');
+      event.target.classList.add('active');
+    }
+    results.innerHTML = '';
+    fetchResource(event.target.innerText);
+  })
+);
