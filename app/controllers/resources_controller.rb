b3ac1195c108f31e00ee9a0376c8d3826e23751f
Cataline Baert <snuf_kin@yahoo.com>
2019-12-10 07:45:35 +0100

    empty methods

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,14 +1,7 @@
-require 'json'
-require 'open-uri'
-
 class ResourcesController < ApplicationController
   def index
   end
 
   def show
-    url = "https://swapi.co/api/#{params[:id]}"
-    serialized_resource = URI.open(url).read
-    resource = JSON.parse(serialized_resource)
-    @resource = resource['results']
   end
 end
