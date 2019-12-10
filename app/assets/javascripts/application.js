db2dbc2df8bf0f9a9a6a245a61b091b40f2f0998
Cataline Baert <snuf_kin@yahoo.com>
2019-12-10 07:44:06 +0100

    comment js for resources

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,10 @@
 //= require rails-ujs
 //= require_tree .
+
 const resources = document.getElementById('resources');
 
 const results = document.getElementById('results');
 
 const details = document.getElementById('details');
 
-fetchResources();
+// fetchResources();
