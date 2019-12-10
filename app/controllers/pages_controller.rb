4514679e6933cc788f7db5cfd64456deb7f43eab
Cataline Baert <snuf_kin@yahoo.com>
2019-12-10 07:47:32 +0100

    define controller home using services

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,30 +1,13 @@
-require 'json'
-require 'open-uri'
-
 class PagesController < ApplicationController
   def home
-    # get resources types
     base_url = 'https://swapi.co/api/'
-    serialized_resources = URI.open(base_url).read
-    resources = JSON.parse(serialized_resources)
-    @resources = resources.keys
-    # get people
-    people_data = Swapi.get_all 'people'
-    @people = JSON.parse(people_data)['results']
-    # get planets
-    planets_data = Swapi.get_all 'planets'
-    @planets = JSON.parse(planets_data)['results']
-    # get films
-    films_data = Swapi.get_all 'films'
-    @films = JSON.parse(films_data)['results']
-    # get species
-    species_data = Swapi.get_all 'species'
-    @species = JSON.parse(species_data)['results']
-    # get vehicles
-    vehicles_data = Swapi.get_all 'vehicles'
-    @vehicles = JSON.parse(vehicles_data)['results']
-    # get starships
-    starships_data = Swapi.get_all 'starships'
-    @starships = JSON.parse(starships_data)['results']
+
+    resources = CallApiService.new(url: base_url).call
+    @resources_types = resources.keys
+
+    @types = {}
+    resources.each do |key, value|
+      @types[key.to_sym] = CallApiService.new(url: value).call
+    end
   end
 end
