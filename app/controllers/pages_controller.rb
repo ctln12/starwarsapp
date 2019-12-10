20455c1f844483fa38cf7659083cec172743ed61
Cataline Baert <snuf_kin@yahoo.com>
2019-12-10 16:39:04 +0100

    define all controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,20 +3,38 @@ class PagesController < ApplicationController
   end
 
   def people
+    results = FetchDataService.new(type: 'people').call
+    @more_people = results['next']
+    @people = results['results']
   end
 
   def planets
+    results = FetchDataService.new(type: 'planets').call
+    @more_planets = results['next']
+    @planets = results['results']
   end
 
   def films
+    results = FetchDataService.new(type: 'films').call
+    @more_films = results['next']
+    @films = results['results']
   end
 
   def species
+    results = FetchDataService.new(type: 'species').call
+    @more_species = results['next']
+    @species = results['results']
   end
 
   def vehicles
+    results = FetchDataService.new(type: 'vehicles').call
+    @more_vehicles = results['next']
+    @vehicles = results['results']
   end
 
   def starships
+    results = FetchDataService.new(type: 'starships').call
+    @more_starships = results['next']
+    @starships = results['results']
   end
 end
