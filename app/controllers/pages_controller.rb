a39db6afad824ce605f30d852036bc4eabe28dc0
Cataline Baert <snuf_kin@yahoo.com>
2019-12-10 19:51:58 +0100

    modify variable more

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,37 +4,37 @@ class PagesController < ApplicationController
 
   def people
     results = FetchDataService.new(type: 'people').call
-    @more_people = results['next']
+    @more = results['next']
     @people = results['results']
   end
 
   def planets
     results = FetchDataService.new(type: 'planets').call
-    @more_planets = results['next']
+    @more = results['next']
     @planets = results['results']
   end
 
   def films
     results = FetchDataService.new(type: 'films').call
-    @more_films = results['next']
+    @more = results['next']
     @films = results['results']
   end
 
   def species
     results = FetchDataService.new(type: 'species').call
-    @more_species = results['next']
+    @more = results['next']
     @species = results['results']
   end
 
   def vehicles
     results = FetchDataService.new(type: 'vehicles').call
-    @more_vehicles = results['next']
+    @more = results['next']
     @vehicles = results['results']
   end
 
   def starships
     results = FetchDataService.new(type: 'starships').call
-    @more_starships = results['next']
+    @more = results['next']
     @starships = results['results']
   end
 end
