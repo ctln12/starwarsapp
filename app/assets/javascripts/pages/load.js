a4dd4ea2681b3d489a31f6436ef2435d737b0f7c
Cataline Baert <snuf_kin@yahoo.com>
2019-12-13 16:09:15 +0100

    rename file

--- app/assets/javascripts/pages/load.js
+++ app/assets/javascripts/pages/load.js
@@ -1,25 +1,3 @@
-const modalsSection = document.getElementById('modals');
-
-const createHtmlModal = (dataId, title, content) => {
-  const htmlModal = `
-    <div class="modal fade" id="${dataId}" tabindex="-1" role="dialog" aria-labelledby="modalScrollableTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-scrollable" role="document">
-        <div class="modal-content bg-dark">
-          <div class="modal-header">
-            <h5 class="modal-title" id="modalScrollableTitle">${title.toUpperCase()}</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            ${content}
-          </div>
-        </div>
-      </div>
-    </div>`;
-  return htmlModal;
-};
-
 const loadDetails = (data) => {
   const detailsArray = [];
   data.results.forEach((result) => {
