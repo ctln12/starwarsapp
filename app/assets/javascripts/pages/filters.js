6da6306edb4a7d34296a5dcf1f7beb8c876c80b8
Cataline Baert <snuf_kin@yahoo.com>
2019-12-13 14:20:58 +0100

    remove reload of details on filter click

--- app/assets/javascripts/pages/filters.js
+++ app/assets/javascripts/pages/filters.js
@@ -2,15 +2,12 @@ const addClickEvent = (filters) => {
   filters.forEach((filter) => {
     filter.addEventListener('click', (event) => {
       itemsRow.innerText = '';
-      modalsSection.innerText = '';
       const type = event.target.innerText.toLowerCase();
       if (type !== 'all') {
         callApi(`${base_url}${type}/`, insertItemsNames);
-        fetchAllItems(type, NB_PAGES[type], loadDetails);
         console.log('clicked ', type.toUpperCase());
       } else {
         fetchAllItemsNames(displayAllItemsNames);
-        fetchAllDetails(loadDetails);
         console.log('clicked ALL');
         moreButton.setAttribute('hidden', '');
       }
@@ -34,7 +31,6 @@ const insertFilters = (data) => {
     const htmlFilter = `<p class="filter" data-url="${data[filter]}">${filter.toUpperCase()}</p>`;
     filters.insertAdjacentHTML('beforeEnd', htmlFilter);
   });
-  // fetchAllItemsNames(displayAllItemsNames);
   const filtersList = document.querySelectorAll('.filter');
   addClickEvent(filtersList);
 };
