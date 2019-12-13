e887a2002064e92148f62862296b77cd92623668
Cataline Baert <snuf_kin@yahoo.com>
2019-12-13 13:12:07 +0100

    move add event listener to separate file and call in application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,8 +2,6 @@
 //= require rails-ujs
 //= require_tree .
 
-// ROOT PATH
-
 // Search Section
 
 // Div in which are stored all the items
@@ -12,23 +10,7 @@ const searchData = document.getElementById('search-data');
 const searchResults = document.getElementById('search-results');
 // Button that lauches the query on click
 const searchButton = document.getElementById('search-button');
-// Add click event to search button
-searchButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  const activeFilter = document.querySelector('.filter.active');
-  if (activeFilter !== null) {
-    activeFilter.classList.remove('active');
-  }
-  const allItems = [];
-  const searchItems = document.querySelectorAll('#result-item');
-  searchItems.forEach((item) => {
-    allItems.push(item.innerText);
-  });
-  const searchValue = document.getElementById('search-input').value;
-  const searcher = new FuzzySearch(allItems);
-  const result = searcher.search(searchValue);
-  displayResults(result);
-});
+addSearchEvent();
 
 // Resources Section
 const NB_PAGES = { 'people': 10, 'planets': 8, 'films': 2, 'species': 5, 'vehicles': 5, 'starships': 5 };
@@ -52,18 +34,5 @@ const moreCol = document.getElementById('more-col');
 
 // More button that display more resources
 const moreButton = document.querySelector('#more-button');
+addMoreEvent();
 
-// Add click event to more button
-moreButton.addEventListener('click', (event) => {
-  const activeFilter = document.querySelector('.filter.active');
-  const type = document.querySelector('.filter.active').innerText.toLowerCase();
-  const nb = parseInt(moreButton.getAttribute('data-page'), 10);
-  if (activeFilter.innerText === 'ALL') {
-    callApi(base_url, insertMoreItemsNames);
-    console.log('clicked more ALL');
-  } else if (activeFilter.innerText !== 'FILMS') {
-    callApi(`${base_url}${type}/?page=${nb+1}`,insertItemsNames);
-    moreButton.setAttribute('data-page', nb+1);
-    console.log('clicked more ', type);
-  }
-});
---
+++ app/assets/javascripts/pages/events.js
@@ -0,0 +1,35 @@
+// Add click event to search button
+const addSearchEvent = () => {
+  searchButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const activeFilter = document.querySelector('.filter.active');
+    if (activeFilter !== null) {
+      activeFilter.classList.remove('active');
+    }
+    const allItems = [];
+    const searchItems = document.querySelectorAll('#result-item');
+    searchItems.forEach((item) => {
+      allItems.push(item.innerText);
+    });
+    const searchValue = document.getElementById('search-input').value;
+    const searcher = new FuzzySearch(allItems);
+    const result = searcher.search(searchValue);
+    displayResults(result);
+  });
+}
+// Add click event to more button
+const addMoreEvent = () => {
+  moreButton.addEventListener('click', (event) => {
+  const activeFilter = document.querySelector('.filter.active');
+  const type = document.querySelector('.filter.active').innerText.toLowerCase();
+  const nb = parseInt(moreButton.getAttribute('data-page'), 10);
+  if (activeFilter.innerText === 'ALL') {
+    callApi(base_url, insertMoreItemsNames);
+    console.log('clicked more ALL');
+  } else if (activeFilter.innerText !== 'FILMS') {
+    callApi(`${base_url}${type}/?page=${nb+1}`,insertItemsNames);
+    moreButton.setAttribute('data-page', nb+1);
+    console.log('clicked more ', type);
+  }
+  });
+}
