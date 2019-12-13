27fceb999f253483a94bfef4381f0f1b9e90553a
Cataline Baert <snuf_kin@yahoo.com>
2019-12-13 07:17:42 +0100

    delete unnecessary file

--- test/controllers/resources_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ResourcesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get resources_index_url
-    assert_response :success
-  end
-
-end
