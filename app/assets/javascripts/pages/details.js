c1581848a7fdb1636f401c80d6880bd0eb1d61d5
Cataline Baert <snuf_kin@yahoo.com>
2019-12-13 13:00:51 +0100

    rename file

---
+++ app/assets/javascripts/pages/details.js
@@ -0,0 +1,44 @@
+const modalsSection = document.getElementById('modals');
+
+const createHtmlModal = (dataId, title, content) => {
+  const htmlModal = `
+    <div class="modal fade" id="${dataId}" tabindex="-1" role="dialog" aria-labelledby="modalScrollableTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content bg-dark">
+          <div class="modal-header">
+            <h5 class="modal-title" id="modalScrollableTitle">${title.toUpperCase()}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ${content}
+          </div>
+        </div>
+      </div>
+    </div>`;
+  return htmlModal;
+};
+
+const loadDetails = (data) => {
+  const detailsArray = [];
+  data.results.forEach((result) => {
+    const resultKeys = Object.keys(result);
+    const name = result['name'] || result['title'];
+    const dataId = name.replace(/\W/g,'-');
+    let htmlModal = '';
+    let title = '';
+    let htmlContent = '';
+
+    resultKeys.forEach((resultKey) => {
+      if (resultKey === 'name' || resultKey === 'title') {
+        title = result[resultKey];
+      } else if (resultKey !== 'created' && resultKey !== 'edited' && resultKey !== 'url') {
+        htmlContent += `<p>${resultKey.replace('_', ' ').replace(/^\w/, c => c.toUpperCase())}: ${result[resultKey]}</p>`;
+      }
+    });
+    htmlModal = createHtmlModal(dataId, title, htmlContent);
+    modalsSection.insertAdjacentHTML('beforeEnd', htmlModal);
+    console.log('added modal');
+  });
+};
