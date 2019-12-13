72551b94b965dd9b68eac779fd41c6fe3dfbbdb9
Cataline Baert <snuf_kin@yahoo.com>
2019-12-13 07:16:47 +0100

    delete unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/people', to: 'pages#people'
-  get '/planets', to: 'pages#planets'
-  get '/films', to: 'pages#films'
-  get '/species', to: 'pages#species'
-  get '/vehicles', to: 'pages#vehicles'
-  get '/starships', to: 'pages#starships'
-  # others
-  get '/resources', to: 'resources#index', as: :resources
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
