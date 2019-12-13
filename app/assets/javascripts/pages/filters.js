5ba7e6f2d9d51c1e099e7f337a49f66fb1bf3e30
Cataline Baert <snuf_kin@yahoo.com>
2019-12-13 14:28:05 +0100

    remove add click event function

--- app/assets/javascripts/pages/filters.js
+++ app/assets/javascripts/pages/filters.js
@@ -1,29 +1,3 @@
-const addClickEvent = (filters) => {
-  filters.forEach((filter) => {
-    filter.addEventListener('click', (event) => {
-      itemsRow.innerText = '';
-      const type = event.target.innerText.toLowerCase();
-      if (type !== 'all') {
-        callApi(`${base_url}${type}/`, insertItemsNames);
-        console.log('clicked ', type.toUpperCase());
-      } else {
-        fetchAllItemsNames(displayAllItemsNames);
-        console.log('clicked ALL');
-        moreButton.setAttribute('hidden', '');
-      }
-      const activeFilter = document.querySelector('.filter.active');
-      if (activeFilter === null) {
-        event.target.classList.add('active');
-        moreButton.setAttribute('hidden', '');
-      } else {
-        activeFilter.classList.remove('active');
-        event.target.classList.add('active');
-      }
-      moreButton.setAttribute('data-page', '1');
-    });
-  });
-};
-
 const insertFilters = (data) => {
   filters.insertAdjacentHTML('afterBegin', '<p class="filter active">ALL</p>');
   const filtersArray = Object.keys(data);
